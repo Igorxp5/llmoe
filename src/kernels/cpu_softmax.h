e542d3d82ee93872f909d7ac3598a715646bc2f4
Igorxp5 <rogixp5@gmail.com>
2026-07-30 22:47:20 +0000

    perf(kernels): vectorize cpu_softmax_row_max with AVX-512

--- src/kernels/cpu_softmax.h
+++ src/kernels/cpu_softmax.h
@@ -1,18 +1,38 @@
 #ifndef KERNELS_CPU_SOFTMAX_H
 #define KERNELS_CPU_SOFTMAX_H
 
+#include <immintrin.h>
 #include <math.h>
 
 #include "kernels/kernels.h"
 
 /* Find the maximum value in `input` of length `dim` (numerical-stability
- * helper for softmax). */
+ * helper for softmax). Uses a tree-structured reduce: 16-wide AVX-512
+ * chunks are reduced to scalars, then those scalars are reduced
+ * iteratively until one value remains. */
 static inline float cpu_softmax_row_max(const float *input, size_t dim)
 {
-    float mx = input[0];
-    for (size_t r = 1; r < dim; ++r)
-        if (input[r] > mx) mx = input[r];
-    return mx;
+    const float *cur_input = input;
+    size_t cur_dim = dim;
+    float maxes[dim / 16 + 1];
+
+    do {
+        size_t nmax = 0;
+        size_t r = 0;
+        for (; r + 16 <= cur_dim; r += 16) {
+            __m512 v = _mm512_loadu_ps(cur_input + r);
+            maxes[nmax++] = _mm512_reduce_max_ps(v);
+        }
+        if (r < cur_dim) {
+            __mmask16 mask = (1ULL << (cur_dim - r)) - 1;
+            __m512 v = _mm512_mask_loadu_ps(_mm512_set1_ps(-INFINITY), mask, cur_input + r);
+            maxes[nmax++] = _mm512_reduce_max_ps(v);
+        }
+        cur_input = maxes;
+        cur_dim = nmax;
+    } while (cur_dim > 1);
+
+    return maxes[0];
 }
 
 /* Stable softmax: output[i] = exp(input[i] - max) / sum(exp(input - max)).
