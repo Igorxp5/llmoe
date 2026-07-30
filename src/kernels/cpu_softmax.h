a766d41b081beae6358a2f247cab85174c233167
Igorxp5 <rogixp5@gmail.com>
2026-07-30 23:49:32 +0000

    perf: optimize cpu_softmax with OpenMP parallel reduce and AVX-512 division

--- src/kernels/cpu_softmax.h
+++ src/kernels/cpu_softmax.h
@@ -37,16 +37,28 @@ static inline float cpu_softmax_row_max(const float *input, size_t dim)
 
 /* Stable softmax: output[i] = exp(input[i] - max) / sum(exp(input - max)).
  * Both vectors have length `dim`. */
+static inline void cpu_softmax_row_norm(float *output, size_t dim, float sum)
+{
+    __m512 vsum = _mm512_set1_ps(sum);
+    size_t r = 0;
+    for (; r + 16 <= dim; r += 16) {
+        __m512 v = _mm512_loadu_ps(output + r);
+        _mm512_storeu_ps(output + r, _mm512_div_ps(v, vsum));
+    }
+    for (; r < dim; ++r)
+        output[r] /= sum;
+}
+
 static inline void cpu_softmax(float *output, const float *input, size_t dim)
 {
     float mx = cpu_softmax_row_max(input, dim);
     float sum = 0.0f;
+    #pragma omp parallel for schedule(static) reduction(+:sum)
     for (size_t r = 0; r < dim; ++r) {
         output[r] = expf(input[r] - mx);
         sum += output[r];
     }
-    for (size_t r = 0; r < dim; ++r)
-        output[r] /= sum;
+    cpu_softmax_row_norm(output, dim, sum);
 }
 
 #endif /* KERNELS_CPU_SOFTMAX_H */
